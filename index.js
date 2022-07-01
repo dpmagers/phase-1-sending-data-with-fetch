342d49622a130bb1897093557e8bf85d6830b451
Daniel Magers <daniel.magers@gmail.com>
2022-07-01 03:10:26 -0500

    commit tree fa7c3af75de2ba5ea42fcd9e5eb9f81752b5043d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarok!");
+        console.log(error.message);
+    });
